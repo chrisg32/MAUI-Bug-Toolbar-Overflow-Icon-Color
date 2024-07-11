00080fd7b6dddc0e8f8a7f822a4e97bf51fd8583
Chris Gonzales <chrisg32@users.noreply.github.com>
2024-07-11 12:32:41 -0500

    Commit repro project

---
+++ MAUIToolbarDemo/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MAUIToolbarDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //shell
+            MainPage = new AppShell();
+            
+            //no shell
+            //MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
